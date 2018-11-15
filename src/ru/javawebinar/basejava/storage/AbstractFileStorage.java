e7f7d0d0a4680b5ea340c7695e4e0ed72620aed0
Denis A. Volkov <vda@bankermak.ru>
2018-11-15 17:11:58 +0500

    prepare AbstractFileStorage

---
+++ src/ru/javawebinar/basejava/storage/AbstractFileStorage.java
@@ -0,0 +1,76 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private final File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory required not null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected boolean isExist(File searchKey) {
+        return searchKey.exists();
+    }
+
+    @Override
+    protected void doSave(Resume resume, File searchKey) {
+        try {
+            searchKey.createNewFile();
+            doWrite(resume, searchKey);
+        } catch (IOException e) {
+            throw new StorageException("IO error", searchKey.getName(), e);
+        }
+    }
+
+    @Override
+    protected Resume doGet(File searchKey) {
+        return null;
+    }
+
+    @Override
+    protected void doDelete(File searchKey) {
+
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, File searchKey) {
+
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    abstract void doWrite(Resume resume, File searchKey);
+}
