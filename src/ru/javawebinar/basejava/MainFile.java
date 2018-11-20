dbf21391000d9fea43eb4ecaff0c5590fc8715da
Denis A. Volkov <vda@bankermak.ru>
2018-11-20 11:19:25 +0500

    add recursive output with space MainFile

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.Arrays;
 
 public class MainFile {
+    private static StringBuilder prefix = new StringBuilder();
+
     public static void main(String[] args) {
         File dir = new File("./src");
         listDir(dir);
@@ -12,14 +14,31 @@ public class MainFile {
     private static void listDir(File dir) {
         File[] list = dir.listFiles();
         if (list != null) {
-            Arrays.sort(list);
+            Arrays.sort(list, MainFile::compare);
             for (File file : list) {
                 if (file.isDirectory()) {
+                    System.out.println(prefix + file.getName());
+                    prefix.append(" ");
                     listDir(file);
                 } else {
-                    System.out.println(file.getName());
+                    System.out.println(prefix + file.getName());
                 }
             }
+        }
+
+        int length = prefix.length();
+        if (length > 0) {
+            prefix.delete(length - 1, length);
+        }
+    }
+
+    private static int compare(File o1, File o2) {
+        if (o1.isDirectory()) {
+            return o2.isDirectory() ? o1.compareTo(o2) : -1;
+        } else if (o2.isDirectory()) {
+            return 1;
+        } else {
+            return o1.compareTo(o2);
         }
     }
 }
